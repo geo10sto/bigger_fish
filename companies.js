0de8ebe98d05925647f6a608165c271758cac2cd
Cristi Stoica <crististoik@gmail.com>
2013-03-03 01:13:15 +0200

    companies json

---
+++ companies.js
@@ -0,0 +1,49 @@
+var servicesProvided = {
+    [{
+        type:"Services", 
+        subtypes = [
+        {
+            type:"Internet Service Provider", 
+            service: "Internet connection services",
+        },
+        {
+            type:"Transport",
+            service: "Transportation"
+        }
+        ]
+    },
+    {
+        type:"Retail", 
+        subtypes = [
+        {
+            type:"Automotive and parts",
+            service:"Cars and car parts sales"
+        },
+        {
+            type:"Electronics",
+            service:"Electronic equipment sales"
+        }
+        ],
+    },
+    {
+        type:"State Companies", 
+        subtypes = [
+        {
+            type:"Electricity Company",
+            service: "Electricity"
+        },
+        {
+            type:"Water Supply National Company",
+            service:"Water supply"
+        },
+        {
+            type:"National Railways",
+            service:"Railway transportation"
+        },
+        {
+            type:"National Airways",
+            service:"National airway transportation"
+        }
+        ],
+    }
+}
