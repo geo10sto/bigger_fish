2114218d08c7769706109bc473e59a2fa59c50ab
Cristi Stoica <crististoik@gmail.com>
2013-03-03 02:08:21 +0200

    relations and metrics json

---
+++ metrics.js
@@ -0,0 +1,63 @@
+var userMetrics = {
+    metrics:[
+    {
+        name:"Money",
+        init:5000,
+        unit:100,
+        proc:2
+    },
+    {
+        name:"Notoriety",
+        init:100,
+        unit:20,
+        proc:5
+    },
+    {
+        name:"Reputation",
+        init:100,
+        unit:20,
+        proc:2
+    },
+    {
+        name:"Influence",
+        init:100,
+        unit:20,
+        proc:2
+    },
+    {
+        name:"Morality",
+        init:100,
+        unit:20,
+        proc:2
+    }
+    ]
+}
+
+var companyMetrics = {
+    metrics:[
+    {
+        name:"Used Potential",
+        init:0,
+        unit:10,
+        max:100,
+    },
+    {
+        name:"Net Income",
+        init:0,
+        unit:100,
+        proc:2
+    },
+    {
+        name:"Employees",
+        init:2,
+        unit:1,
+        proc:2
+    },
+    {
+        name:"Cashflow",
+        init:0,
+        unit:10,
+        proc:2
+    }
+    ]
+}
---
+++ relations.js
@@ -0,0 +1,155 @@
+var serviceRelations = {
+    services:[
+    {
+        company:"any",
+        service:"Internet connection services",
+        maxPurchases:2,
+        affectCompany:[
+        {
+            metric:"Used Potential",
+            raise:10,
+        },
+        {
+            metric:"Profitability",
+            raise:10,
+        },]
+        affectUser:
+        {
+            metric:"Reputation",
+            raise:10
+        }
+    },
+    {
+        company:"any",
+        service:"Electricity",
+        maxPurchases:2,
+        affectCompany:[
+        {
+            metric:"Used Potential",
+            raise:10,
+        },
+        {
+            metric:"Profitability",
+            raise:10,
+        },]
+        affectUser:
+        {
+            metric:"Reputation",
+            raise:10
+        }
+    },
+    {
+        company:"any",
+        service:"Water supply",
+        maxPurchases:2,
+        affectCompany:[
+        {
+            metric:"Used Potential",
+            raise:10,
+        },
+        {
+            metric:"Profitability",
+            raise:10,
+        },]
+        affectUser:
+        {
+            metric:"Reputation",
+            raise:10
+        }
+    },
+    {
+        company:"Transport",
+        service:"Automotive and parts",
+        maxPurchases:2,
+        affectCompany:[
+        {
+            metric:"Used Potential",
+            raise:10,
+        },
+        {
+            metric:"Profitability",
+            raise:10,
+        },]
+        affectUser:
+        {
+            metric:"Reputation",
+            raise:10
+        }
+    },
+    {
+        company:"Automotive and parts",
+        service:"Transportation",
+        maxPurchases:2,
+        affectCompany:[
+        {
+            metric:"Used Potential",
+            raise:10,
+        },
+        {
+            metric:"Profitability",
+            raise:10,
+        },]
+        affectUser:
+        {
+            metric:"Reputation",
+            raise:10
+        }
+    },
+    {
+        company:"Electronics",
+        service:"Transport",
+        maxPurchases:2,
+        affectCompany:[
+        {
+            metric:"Used Potential",
+            raise:10,
+        },
+        {
+            metric:"Profitability",
+            raise:10,
+        },]
+        affectUser:
+        {
+            metric:"Reputation",
+            raise:10
+        }
+    },
+    {
+        company:"Automotive and parts",
+        service:"Railway transportation",
+        maxPurchases:2,
+        affectCompany:[
+        {
+            metric:"Used Potential",
+            raise:10,
+        },
+        {
+            metric:"Profitability",
+            raise:10,
+        },]
+        affectUser:
+        {
+            metric:"Reputation",
+            raise:10
+        }
+    },
+    {
+        company:"Electronics",
+        service:"Railway transportation",
+        maxPurchases:2,
+        affectCompany:[
+        {
+            metric:"Used Potential",
+            raise:10,
+        },
+        {
+            metric:"Profitability",
+            raise:10,
+        },]
+        affectUser:
+        {
+            metric:"Reputation",
+            raise:10
+        }
+    }
+    ]
