2e10f53d734ec9f4726386852a884f2510d9474f
alexbardas <alexbardas@gmail.com>
2013-03-03 07:17:37 +0200

    Added company page and good image on header

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, redirect, url_for, session, request, render_template
+from flask import Flask, session, jsonify, redirect, url_for, session, request, render_template
 from flask_oauth import OAuth
 import json
 
@@ -23,31 +23,36 @@ facebook = oauth.remote_app('facebook',
     request_token_params={'scope': 'email'}
 )
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
     #return redirect(url_for('login'))
 
-@app.route('/login', methods=['POST'])
+@app.route('/company')
+def company():
+    return render_template('company.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     if request.method == 'POST':
+#         import ipdb; ipdb.set_trace()
+#         print facebook.authorize(callback=url_for('facebook_authorized'))
+#         return jsonify({"status": "OK", "message": "loggedIn"})
+
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        return jsonify({"status": "OK", "message": "loggedIn"})
+    return facebook.authorize(callback='http://127.0.0.1:5000'+url_for('wizard',
+        next=request.args.get('next') or request.referrer or None,
+        _external=False))
+
 
 @app.route('/wizard')
-def wizard():
-    return render_template('wizard.html')
-
-# @app.route('/login')
-# def login():
-#     return facebook.authorize(callback=url_for('facebook_authorized',
-#         next=request.args.get('next') or request.referrer or None,
-#         _external=True))
-
-
-@app.route('/login/authorized')
 @facebook.authorized_handler
-def facebook_authorized(resp):
+def wizard(resp):
     if resp is None:
         return 'Access denied: reason=%s error=%s' % (
             request.args['error_reason'],
@@ -55,14 +60,15 @@ def facebook_authorized(resp):
         )
     session['oauth_token'] = (resp['access_token'], '')
     me = facebook.get('/me')
-    return 'Logged in as id=%s name=%s redirect=%s' % \
-        (me.data['id'], me.data['name'], request.args.get('next'))
 
+    request.data = {'picture': 'http://graph.facebook.com/%s/picture?type=large' % me.data['username'],
+        'first_name': me.data['first_name'], 'last_name': me.data['last_name']}
+
+    return render_template('wizard.html', **request.data)
 
 @facebook.tokengetter
 def get_facebook_oauth_token():
     return session.get('oauth_token')
-
 
 if __name__ == '__main__':
     app.run()
